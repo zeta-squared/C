1da494ad982ba76c76405d639d78fb5581bd8eae
Zeaiter Zeaiter <zeaiter.zeaiter@sydney.edu.au>
2022-05-08 14:25:35 +1000

    working through chapter 8

---
+++ Chapter 8/interest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define NUM_RATES ((int) (sizeof(balance) / sizeof(balance[0])))
+#define INITIAL_BALANCE 100.00
+
+int main(void)
+{
+	int rate, years;
+	double balance[5];
+
+	printf("Enter interest rate: ");
+	scanf("%d", &rate);
+	printf("Enter number of years: ");
+	scanf("%d", &years);
+
+	printf("\nYears");
+	for (int i = 0; i < NUM_RATES; i++) {
+		printf("%6d%%", rate + i);
+		balance[i] = INITIAL_BALANCE;
+	}
+	printf("\n");
+
+	for (int i = 0; i < years; i++) {
+		printf("%3d", i+1);
+		printf("    ");
+		for (int j = 0; j < NUM_RATES; j++) {
+			balance[j] += balance[j] * ((rate + j) / 100.0);
+			printf("%7.2f", balance[j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
