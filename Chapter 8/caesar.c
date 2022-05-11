18084cf215eef9068b7c6cc85a38c34fede159c7
Zeaiter Zeaiter <zeaiter.zeaiter@sydney.edu.au>
2022-05-12 00:54:27 +1000

    completed programming project 15 in chapter 8

---
+++ Chapter 8/caesar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define MAX_CH 80
+
+int main(void)
+{
+    int i = 0, shift, msg_length = 0;
+    char ch, message[MAX_CH] = {0};
+
+    printf("Enter message to be encrypted: ");
+    while ((ch = getchar()) != '\n') {
+        message[i] = ch;
+        i++;
+        msg_length++;
+    }
+
+    printf("Enter shift amount (1-25): ");
+    scanf("%d", &shift);
+    printf("Ecrypted message: ");
+    for (i = 0; i < msg_length; i++) {
+        if (message[i] >= 'A' && message[i] <= 'Z') {
+            message[i] = (((message[i] - 'A') + shift) % 26) + 'A';
+        } else if (message[i] >= 'a' && message[i] <= 'z') {
+            message[i] = (((message[i] - 'a') + shift) % 26) + 'a';
+        }
+        putchar(message[i]);
+    }
+    putchar('\n');
+
+    return 0;
+}
