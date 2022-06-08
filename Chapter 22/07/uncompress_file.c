706bd2d12075b91114801ae5e8939eaf9174cb46
Zeaiter Zeaiter <zeaiter.zeaiter@sydney.edu.au>
2022-06-08 11:30:16 +1000

    finished up to programming project 8 in chapter 22

--- Chapter 22/07/uncompress_file.c
+++ Chapter 22/07/uncompress_file.c
@@ -5,49 +5,40 @@
 int main(int argc, char *argv[])
 {
     int i;
-    long int position = 0;
-    unsigned char count;
-    unsigned char ch;
     char *filename;
-    FILE *fp, *wp;
+    unsigned char pair[2];
+    FILE *fpin, *fpout;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: uncompress_file filename\n");
         exit(EXIT_FAILURE);
     }
 
-    if ((fp = fopen(argv[1], "rb")) == NULL) {
+    if ((fpin = fopen(argv[1], "rb")) == NULL) {
         fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    filename = malloc(strlen(argv[1]) + 13);
+    if ((filename = malloc(strlen(argv[1]) + 13)) == NULL) {
+        fprintf(stderr, "Error: memory heap exhausted.\n");
+        exit(EXIT_FAILURE);
+    }
     snprintf(filename, strlen(argv[1]) - 2, "%s", argv[1]);
     strcat(filename, "uncompress_file");
-    if ((wp = fopen(filename, "w+")) == NULL) {
-        fprintf(stderr, "Error: cannot create file %s\n", filename);
+
+    if ((fpout = fopen(filename, "w+")) == NULL) {
+        fprintf(stderr, "Error: cannot open file %s\n", filename);
         exit(EXIT_FAILURE);
     }
 
-    /*
-    while (fread(&count, sizeof(unsigned int), 1, fp) > 0) {
-        fread(&ch, sizeof(unsigned int), 1, fp);
-        printf("%u\n%c\n", count, ch);
-    }
-    */
-
-    while (fread(&ch, sizeof(unsigned char), 1, fp) > 0) {
-        if (position % 2 == 0)
-            count = ch;
-        else {
-            for (i = 0; i < count; i++)
-                fwrite(&ch, sizeof(unsigned char), 1, wp);
-        }
-        position++;
+    free(filename);
+    while ((fread(pair, sizeof(unsigned char), 2, fpin)) > 0) {
+        for (i = 0; i < pair[0]; i++)
+            fprintf(fpout, "%c", pair[1]);
     }
 
-    fclose(fp);
-    fclose(wp);
+    fclose(fpin);
+    fclose(fpout);
 
     return 0;
 }
